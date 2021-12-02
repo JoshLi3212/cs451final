a65ae763cbe8e7350b566c8269fbc38c915ec90a
a4qin <a4qin@student.cs.uwaterloo.ca>
2021-12-01 21:29:57 -0500

    uso done

--- historicalstocks.py
+++ historicalstocks.py
@@ -7,22 +7,22 @@ import sys
 API_KEY = "DH8U3449SHQ3O38F"
 
 
-f = open("spy.csv", "a")
+f = open("uso.csv", "a")
 
 
 with requests.Session() as s:
-    for i in range(20, 24):
+    for i in range(23, 24):
         print(i)
         year = i // 12 + 1
         month = i % 12 + 1
         querydate = "year{}month{}".format(year, month)
         print(querydate)
-        CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=SPY&interval=60min&slice={}&apikey=DH8U3449SHQ3O38F'.format(querydate)
+        CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=USO&interval=60min&slice={}&apikey=DH8U3449SHQ3O38F'.format(querydate)
         download = s.get(CSV_URL)
         decoded_content = download.content.decode('utf-8')
         cr = csv.reader(decoded_content.splitlines(), delimiter=',')
         my_list = list(cr)
-        for row in my_list:
+        for row in my_list[1:]:
             sys.stdout = f
-            print("{}, {}, {}, {}, {}, {}".format(row[0], row[1], row[2], row[3], row[4], row[5]))
+            print("{},{},{},{},{},{}".format(row[0], row[1], row[2], row[3], row[4], row[5]))
             sys.stdout = sys.__stdout__
