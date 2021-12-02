9123a42ba45b88c1d13a3a711a7e0d778829c288
a4qin <a4qin@student.cs.uwaterloo.ca>
2021-12-01 21:44:42 -0500

    downloaded stocks

--- historicalstocks.py
+++ historicalstocks.py
@@ -7,17 +7,17 @@ import sys
 API_KEY = "DH8U3449SHQ3O38F"
 
 
-f = open("uso.csv", "a")
+f = open("gld.csv", "a")
 
 
 with requests.Session() as s:
-    for i in range(23, 24):
+    for i in range(20, 24):
         print(i)
         year = i // 12 + 1
         month = i % 12 + 1
         querydate = "year{}month{}".format(year, month)
         print(querydate)
-        CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=USO&interval=60min&slice={}&apikey=DH8U3449SHQ3O38F'.format(querydate)
+        CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=SPY&interval=60min&slice={}&apikey=DH8U3449SHQ3O38F'.format(querydate)
         download = s.get(CSV_URL)
         decoded_content = download.content.decode('utf-8')
         cr = csv.reader(decoded_content.splitlines(), delimiter=',')
